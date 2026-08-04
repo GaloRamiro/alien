e7f452641868ad3e11a8b7742f3b4f44cb384b53
GaloRamiro <g_cisneros_23@hotmail.es>
2026-08-04 13:28:55 -0500

    Crear clase Alien con atributos, validaciones y métodos básicos

--- src/com/krakedev/aliens/Alien.java
+++ src/com/krakedev/aliens/Alien.java
@@ -35,4 +35,54 @@ public class Alien {
 
 		this.precioTotal = 0;
 	}
+	
+	public int getTamanio() {
+		return tamanio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getNumeroOjos() {
+		return numeroOjos;
+	}
+
+	public int getNumeroBrazos() {
+		return numeroBrazos;
+	}
+
+	public int getNumeroPies() {
+		return numeroPies;
+	}
+
+	public double getPrecioExtremidad() {
+		return precioExtremidad;
+	}
+
+	public double getPrecioOjo() {
+		return precioOjo;
+	}
+
+	public double getPrecioCuerpo() {
+		return precioCuerpo;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+	
+	public void imprimir() {
+		System.out.println("========== ALIEN ==========");
+		System.out.println("Tamaño: " + tamanio + " cm");
+		System.out.println("Color: " + color);
+		System.out.println("Número de ojos: " + numeroOjos);
+		System.out.println("Número de brazos: " + numeroBrazos);
+		System.out.println("Número de pies: " + numeroPies);
+		System.out.println("Precio del cuerpo: $" + precioCuerpo);
+		System.out.println("Precio por extremidad: $" + precioExtremidad);
+		System.out.println("Precio por ojo: $" + precioOjo);
+		System.out.println("Precio total: $" + precioTotal);
+		System.out.println("===========================");
+	}
 }
